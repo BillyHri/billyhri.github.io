63d0bf6745df5cbcdae0bb8de6974223dbf0e131
Billy H <hri0001@outlook.com>
2024-10-05 21:28:55 +1000

    a2

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -7,7 +7,7 @@ const CONFIG = {
   /**
    * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
    */
-  base: '/',
+  base: '//',
   projects: {
     github: {
       display: true, // Display GitHub projects?
