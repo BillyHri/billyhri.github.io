700df43db146660bb1b6c902c8f6dd1de418304b
Billy H <hri0001@outlook.com>
2024-10-05 22:15:30 +1000

    a11

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -81,7 +81,7 @@ const CONFIG = {
       '', //'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
+    'LuaU',
     'Laravel',
     'JavaScript',
     'React.js',
